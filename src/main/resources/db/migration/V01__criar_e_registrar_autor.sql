9c0bbd12360987fc64a26899cf00b040f7b2c4cd
Luis Guilherme Giannina Sante <l.g.g.sante@gmail.com>
2022-09-02 11:42:54 -0300

    Biblioteca V.1.0.0-b-02/09/2022 correção da DB.MIGRATION

--- src/main/resources/db/migration/V01__criar_e_registrar_autor.sql
+++ src/main/resources/db/migration/V01__criar_e_registrar_autor.sql
@@ -2,3 +2,5 @@ CREATE TABLE autor (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(50) NOT NULL
 );
+INSERT INTO autor (nome) VALUES ('Stephen Hawking');
+INSERT INTO autor (nome) VALUES ('Michael Faraday');
--- src/main/resources/db/migration/V02__criar_e_registrar_editora.sql
+++ src/main/resources/db/migration/V02__criar_e_registrar_editora.sql
@@ -2,3 +2,6 @@ CREATE TABLE editora (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(50) NOT NULL
 );
+
+INSERT INTO editora (nome) VALUES ('Editora Intríseca LTDA');
+INSERT INTO editora (nome) VALUES ('Contraponto Editora LTDA');
